c1d976e260501b425dce6839b59194a027fa0e9a
inSaneAman <162028654+inSaneAman@users.noreply.github.com>
2025-08-24 17:45:48 +0530

    error handling

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -8,10 +8,10 @@ export const loginSchema = Yup.object({
 });
 
 export const signupSchema = Yup.object({
-    firstName: Yup.string()
+    first_name: Yup.string()
         .min(2, "First name must be at least 2 characters")
         .required("First name is required"),
-    lastName: Yup.string()
+    last_name: Yup.string()
         .min(2, "Last name must be at least 2 characters")
         .required("Last name is required"),
     email: Yup.string()
