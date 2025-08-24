c54cc6e4a9963133c702e9176fde54cfa9e86369
inSaneAman <162028654+inSaneAman@users.noreply.github.com>
2025-08-24 14:34:48 +0530

    updated store with the reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
-
+import authSliceReducer from './slices/authSlice';
+import leadSliceReducer from './slices/leadSlice';
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authSliceReducer,
+        lead: leadSliceReducer,
+    },
     devTools: true,
 });
 
