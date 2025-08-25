452bcd924a8e1c6cd6d5e527f0437caca5a0afa3
inSaneAman <162028654+inSaneAman@users.noreply.github.com>
2025-08-25 13:23:53 +0530

    Refactor axiosInstance to set baseURL and withCredentials during creation

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -3,9 +3,22 @@ import axios from "axios";
 const BASE_URL =
     "https://lead-management-system-backend-whbe.onrender.com/api/v1/";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true, // include cookies
+});
 
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+// Add a request interceptor to attach token
+axiosInstance.interceptors.request.use(
+    (config) => {
+        // Get token from localStorage (or wherever you store it)
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
