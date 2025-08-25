b8be2d2004e02466c3819e66abede6125cccb894
inSaneAman <162028654+inSaneAman@users.noreply.github.com>
2025-08-25 13:01:41 +0530

    Update BASE_URL in axiosInstance to point to the production API

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5020/api/v1/";
+const BASE_URL =
+    "https://lead-management-system-backend-whbe.onrender.com/api/v1/";
 
 const axiosInstance = axios.create();
 
